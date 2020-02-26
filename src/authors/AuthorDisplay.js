3d581a94523cce0dc8e0e6e9e22eabf10b5e6064
unknown <Michael Lau>
2020-02-26 17:48:47 -0500

    lin

--- src/authors/AuthorDisplay.js
+++ src/authors/AuthorDisplay.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { AuthorCard } from "./AuthorCard";
-import { CardColumns } from "react-bootstrap";
+//import { AuthorCard } from "./AuthorCard";
+//import { CardColumns } from "react-bootstrap";
 
 export function AuthorDisplay(props) {
     return (
         <div className="display">
             <h3>Authors</h3>
-            <CardColumns>
                 {props.authors.map(a => (
-                    <AuthorCard author={a} key={a.id} />
+                   <p>{a.author}</p>
                 ))}
-            </CardColumns>
         </div>
     );
 }
