fbe9041f0a1def1f2f0ad254393ba563e2ba4cdf
unknown <Michael Lau>
2020-02-26 15:18:23 -0500

    aut

--- src/authors/AuthorDisplay.js
+++ src/authors/AuthorDisplay.js
@@ -1,14 +1,16 @@
 import React from "react";
 //import { AuthorCard } from "./AuthorCard";
-//import { CardColumns } from "react-bootstrap";
+import { CardColumns } from "react-bootstrap";
 
 export function AuthorDisplay(props) {
     return (
         <div className="display">
             <h3>Authors</h3>
+            <CardColumns>
                 {props.authors.map(a => (
                     <p>{a.author}</p>
                 ))}
+            </CardColumns>
         </div>
     );
 }
